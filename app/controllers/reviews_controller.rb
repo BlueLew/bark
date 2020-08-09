b62a84b7d6ec4643708eb0d41dd801308b411695
Lewis Cruz <bluelew75@gmail.com>
2020-08-09 16:05:24 -0400

    WIP, delete comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,13 +3,14 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = @restaurant.reviews.new(params[:review].permit(:rating, :comment))
+    @review = @restaurant.reviews.new(review_params)
     @review.user = current_user
     @review.save
     redirect_to restaurant_path(@restaurant)    
   end
 
   def destroy
+    # @restaurant = Restaurant.find_by(params[:restaurant_id])
     @review = Review.find(params[:id])
     if current_user != @review.user
       flash[:danger] = "You can only delete your own review."
@@ -20,4 +21,9 @@ class ReviewsController < ApplicationController
       redirect_to restaurant_path
     end
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:rating, :comment)
+    end
 end
